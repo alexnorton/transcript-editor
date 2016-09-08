601c93417b2c18ee5e6f4c74be94a6bc196149c9
Alex Norton <alexnorton@fastmail.com>
2016-09-08 17:41:34 +0100

    add transcript saving/loading

--- src/components/EditorView.js
+++ src/components/EditorView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button, ButtonToolbar } from 'react-bootstrap';
 import { AutoAffix } from 'react-overlays';
+import { saveAs } from 'file-saver';
 
 import VideoPlayer from './VideoPlayer';
 import TranscriptEditor from './TranscriptEditor';
@@ -11,13 +12,16 @@ class EditorView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      transcript: null,
+      clipName: null,
       initialTranscript: null,
       playing: false,
       currentTime: 0,
     };
     this.onTimeUpdate = this.onTimeUpdate.bind(this);
     this.onTranscriptUpdate = this.onTranscriptUpdate.bind(this);
+    this.loadTranscript = this.loadTranscript.bind(this);
+    this.handleLoadTranscript = this.handleLoadTranscript.bind(this);
+    this.saveTranscript = this.saveTranscript.bind(this);
   }
 
   componentDidMount() {
@@ -25,7 +29,12 @@ class EditorView extends Component {
     fetch(`${window.apiEndpoint}/${this.props.params.videoId}.json`)
       .then(response => response.json())
       .then(json => {
-        this.setState({ initialTranscript: Transcript.fromComma(json) });
+        const transcript = Transcript.fromComma(json);
+        this.setState({
+          initialTranscript: transcript,
+          clipName: json.metadata.RETURN.RESULTS.ITEM.CLIPNAME,
+        });
+        this.transcript = transcript;
       });
   }
 
@@ -35,30 +44,79 @@ class EditorView extends Component {
 
   onTranscriptUpdate(transcript) {
     this.styleManager.setTranscript(transcript);
+    this.transcript = transcript;
+  }
+
+  loadTranscript() {
+    this.fileInput.click();
+  }
+
+  handleLoadTranscript() {
+    const file = this.fileInput.files[0];
+    this.fileInput.value = '';
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = event => {
+      const transcriptJSONString = event.target.result;
+      const transcriptJSON = JSON.parse(transcriptJSONString);
+      const transcript = Transcript.fromJSON(transcriptJSON);
+      this.setState({
+        initialTranscript: transcript,
+      });
+      this.transcript = transcript;
+    };
+
+    fileReader.readAsText(file);
+  }
+
+  saveTranscript() {
+    const blob = new Blob(
+      [JSON.stringify(this.transcript.toJSON(), null, 2)],
+      { type: 'text/plain;charset=utf-8' }
+    );
+    saveAs(blob, 'transcript.json');
   }
 
   render() {
     return (
-      <Row>
-        <Col xs={5}>
-          <AutoAffix
-            viewportOffsetTop={10}
-          >
-            <div>
-              <VideoPlayer
-                videoId={this.props.params.videoId}
-                onTimeUpdate={this.onTimeUpdate}
-              />
-            </div>
-          </AutoAffix>
-        </Col>
-        <Col xs={7}>
-          <TranscriptEditor
-            transcript={this.state.initialTranscript}
-            onTranscriptUpdate={this.onTranscriptUpdate}
-          />
-        </Col>
-      </Row>
+      <div>
+        <Row>
+          <Col xs={12}>
+            <h2 style={{ marginTop: 0 }}>{ this.state.clipName }</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={5}>
+            <AutoAffix
+              viewportOffsetTop={10}
+            >
+              <div>
+                <VideoPlayer
+                  videoId={this.props.params.videoId}
+                  onTimeUpdate={this.onTimeUpdate}
+                />
+                <ButtonToolbar>
+                  <input
+                    type="file"
+                    ref={(c) => { this.fileInput = c; }}
+                    style={{ display: 'none' }}
+                    onChange={this.handleLoadTranscript}
+                  />
+                  <Button onClick={this.loadTranscript}>Load transcript</Button>
+                  <Button onClick={this.saveTranscript}>Save transcript</Button>
+                </ButtonToolbar>
+              </div>
+            </AutoAffix>
+          </Col>
+          <Col xs={7}>
+            <TranscriptEditor
+              transcript={this.state.initialTranscript}
+              onTranscriptUpdate={this.onTranscriptUpdate}
+            />
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Router, Route, hashHistory } from 'react-router';
 import 'whatwg-fetch';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import App from './components/App';
 import EditorView from './components/EditorView';
