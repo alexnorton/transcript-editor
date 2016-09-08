2e455e8014d81fb90f2fa2a7eaad9d2bc707fecb
Alex Norton <alexnorton@fastmail.com>
2016-09-08 15:43:38 +0100

    create Transcript to/from JSON methods - closes #5 #6

--- src/model/Transcript.js
+++ src/model/Transcript.js
@@ -1,4 +1,5 @@
 import Immutable from 'immutable';
+import Ajv from 'ajv';
 
 import TranscriptSegment from './TranscriptSegment';
 import TranscriptWord from './TranscriptWord';
@@ -48,6 +49,126 @@ class Transcript extends TranscriptRecord {
 
     return new Transcript({ speakers, segments });
   }
+
+  static fromJSON(json) {
+    this.validateJSON(json);
+
+    const speakers = new Immutable.Map(json.speakers.map((speaker, index) => [
+      index, new Speaker(speaker),
+    ]));
+
+    const segments = new Immutable.List(
+      json.segments.map(({ speaker, words }) => new TranscriptSegment({
+        speaker,
+        words: new Immutable.List(
+          words.map(({ start, end, text, guid }) => new TranscriptWord({
+            start,
+            end,
+            text,
+            id: guid,
+          }))
+        ),
+      }))
+    );
+
+    return new Transcript({
+      speakers,
+      segments,
+    });
+  }
+
+  static validateJSON(json) {
+    const ajv = new Ajv();
+    const valid = ajv.validate(this.jsonSchema, json);
+    if (!valid) {
+      throw new Error(`invalid transcript JSON:\n${JSON.stringify(ajv.errors, null, 2)}`);
+    }
+    return true;
+  }
+
+  toJSON() {
+    return {
+      speakers: this.speakers.toArray().map(speaker => ({
+        name: speaker.name,
+      })),
+      segments: this.segments.toArray().map(segment => ({
+        words: segment.words.toArray().map(word => ({
+          text: word.text,
+          start: word.start,
+          end: word.end,
+        })),
+        speaker: segment.speaker,
+      })),
+    };
+  }
 }
+
+Transcript.jsonSchema = {
+  type: 'object',
+  properties: {
+    speakers: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: {
+            type: ['string', 'null'],
+          },
+        },
+        required: [
+          'name',
+        ],
+      },
+    },
+    segments: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          speaker: {
+            type: 'integer',
+          },
+          words: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                text: {
+                  type: 'string',
+                },
+                start: {
+                  type: 'number',
+                },
+                end: {
+                  type: 'number',
+                },
+                guid: {
+                  type: 'string',
+                  format: 'uuid',
+                },
+              },
+              required: [
+                'text',
+                'start',
+                'end',
+              ],
+              additionalProperties: false,
+            },
+          },
+        },
+        required: [
+          'speaker',
+          'words',
+        ],
+        additionalProperties: false,
+      },
+    },
+  },
+  additionalProperties: false,
+  required: [
+    'speakers',
+    'segments',
+  ],
+};
 
 export default Transcript;
