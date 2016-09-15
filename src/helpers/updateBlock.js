98a888680a7bb0c4fd557f5dd04cbf33f9e5ba9f
Alex Norton <alexnorton@fastmail.com>
2016-09-15 14:22:10 +0100

    start of state machine based implementation

--- src/helpers/updateBlock.js
+++ src/helpers/updateBlock.js
@@ -1,77 +1,43 @@
 import Immutable from 'immutable';
 import { Entity, CharacterMetadata } from 'draft-js';
+import zipWith from 'lodash.zipwith';
 
 import { TRANSCRIPT_WORD, TRANSCRIPT_SPACE, TRANSCRIPT_PLACEHOLDER }
   from './TranscriptEntities';
 
-const updateBlock = (contentBlock, previousContentBlock) => (
-  contentBlock.characterList.reduce(({ characterList, text }, character, index) => {
-    // Is this the first character?
-    if (!characterList.isEmpty()) {
-      const previousCharacter = characterList.last();
+const states = {
+  state0(input) {
+    const output = { woop: input };
+    const nextState = states.state0;
+    return { output, nextState };
+  },
+};
 
-      // Does the previous character have an entity?
-      if (previousCharacter.entity) {
-        // Does the previous character have a different entity?
-        if (character.entity) {
-          const entity = Entity.get(character.entity);
-          const previousEntity = Entity.get(previousCharacter.entity);
+const run = (characters) => {
+  characters.reduce(({ newCharacters, state }, character, index) => {
+    const nextCharacter = characters[index + 1];
 
-          if (character.entity !== previousCharacter.entity) {
-            // Does the different entity have the same type?
-            if (entity.type === previousEntity.type && entity !== previousEntity) {
-              // Merge the entities
-              Entity.mergeData(previousCharacter.entity, { end: entity.data.end });
+    const { nextState, output } = state(character, nextCharacter, characters);
 
-              return {
-                characterList: characterList.push(
-                  CharacterMetadata.applyEntity(character, previousCharacter.entity)
-                ),
-                text: text + contentBlock.text[index],
-              };
-            }
-          // Otherwise do we have two spaces?
-          } else if (entity.type === TRANSCRIPT_SPACE) {
-            const previousStateEntity = Entity.get(previousContentBlock.getEntityAt(index));
-
-            // Have we just deleted a word?
-            if (previousStateEntity.type === TRANSCRIPT_WORD) {
-              // Add a placeholder
-              return {
-                characterList: characterList
-                  .push(CharacterMetadata.applyEntity(
-                    CharacterMetadata.create(),
-                    Entity.create(
-                      TRANSCRIPT_PLACEHOLDER, 'IMMUTABLE', previousStateEntity.data
-                    )
-                  ))
-                  .push(character),
-                text: `${text}\u200C `,
-              };
-            }
-
-            // Remove a space
-            return {
-              characterList,
-              text,
-            };
-          }
-        }
-      } else {
-        // Set it to the entity of this character
-        return {
-          characterList: characterList
-            .set(-1, CharacterMetadata.applyEntity(previousCharacter, character.entity))
-            .push(character),
-          text: text + contentBlock.text[index],
-        };
-      }
-    }
     return {
-      characterList: characterList.push(character),
-      text: text + contentBlock.text[index],
+      state: nextState,
+      newCharacters: newCharacters.concat(output),
     };
-  }, { characterList: new Immutable.List(), text: '' })
-);
+  }, {
+    state: states.state0,
+    newCharacters: [],
+  });
+};
+
+const updateBlock = (contentBlock, previousContentBlock) => {
+  const characters = zipWith(
+    contentBlock.text.split(''),
+    contentBlock.characterList.toJS(),
+    contentBlock.characterList.toJS().map((c) => Entity.get(c.entity)),
+    (text, characterMetadata, entity) => ({ text, characterMetadata, entity })
+  );
+
+  run(characters);
+};
 
 export default updateBlock;
