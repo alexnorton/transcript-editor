d078632950d23dd62b9a3f4c6c9f6beff79369ab
Alex Norton <alexnorton@fastmail.com>
2016-10-14 14:57:50 +0100

    create webpack configuration to build packaged module

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  entry: './src/components/TranscriptEditor.js',
+  output: {
+    path: './lib',
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          babelrc: false,
+          presets: ['es2015', 'react'],
+          plugins: ['add-module-exports'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
+  },
+  externals: {
+    react: 'react',
+    immutable: 'immutable',
+    ajv: 'ajv',
+    'draft-js': 'draft-js',
+    'node-uuid': 'node-uuid',
+    'lodash.debounce': 'lodash.debounce',
+    'react-bootstrap': 'react-bootstrap',
+  },
+};
