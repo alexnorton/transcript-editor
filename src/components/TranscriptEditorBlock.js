64f3da5ce09b5a1e43c14711fada6f0564c64e23
Alex Norton <alexnorton@fastmail.com>
2016-10-21 12:16:56 +0100

    rewrite component classes as pure functions

--- src/components/TranscriptEditorBlock.js
+++ src/components/TranscriptEditorBlock.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { EditorBlock } from 'draft-js';
 
-class TranscriptEditorBlock extends Component {
-  render() {
-    return (
-      <div className="transcript-editor-block">
-        <div className="transcript-editor-block__speaker"
-          contentEditable={false}
-          style={{
-            MozUserSelect: 'none',
-            WebkitUserSelect: 'none',
-            msUserSelect: 'none',
-          }}
-        >
-          Speaker {this.props.block.data.get('speaker')}
-        </div>
-        <div className="transcript-editor-block__text">
-          <EditorBlock {...this.props} />
-        </div>
-      </div>
-    );
-  }
-}
+const TranscriptEditorBlock = props => (
+  <div className="transcript-editor-block">
+    <div
+      className="transcript-editor-block__speaker"
+      contentEditable={false}
+      style={{
+        MozUserSelect: 'none',
+        WebkitUserSelect: 'none',
+        msUserSelect: 'none',
+      }}
+    >
+      Speaker {props.block.data.get('speaker')}
+    </div>
+    <div className="transcript-editor-block__text">
+      <EditorBlock {...props} />
+    </div>
+  </div>
+);
 
 TranscriptEditorBlock.propTypes = {
   block: React.PropTypes.object,
--- src/components/TranscriptEditorWord.js
+++ src/components/TranscriptEditorWord.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Entity } from 'draft-js';
 
-class TranscriptEditorWord extends Component {
-  static getId(entityKey) {
-    return `word-${entityKey}`;
-  }
-
-  render() {
-    const entity = Entity.get(this.props.entityKey);
-    const titleString = `${entity.data.start.toFixed(2)}`
-      + ` - ${entity.data.end.toFixed(2)}`;
-    return (
-      <span
-        title={titleString}
-        id={`word-${entity.data.id}`}
-        data-start={entity.data.start}
-        data-end={entity.data.end}
-        className="transcript-editor-block__word"
-      >{ this.props.children }</span>
-    );
-  }
-
-}
+const TranscriptEditorWord = ({ entityKey, children }) => {
+  const entity = Entity.get(entityKey);
+  const titleString = `${entity.data.start.toFixed(2)}`
+    + ` - ${entity.data.end.toFixed(2)}`;
+  return (
+    <span
+      title={titleString}
+      id={`word-${entity.data.id}`}
+      data-start={entity.data.start}
+      data-end={entity.data.end}
+      className="transcript-editor-block__word"
+    >{ children }</span>
+  );
+};
 
 TranscriptEditorWord.propTypes = {
   entityKey: React.PropTypes.string,
