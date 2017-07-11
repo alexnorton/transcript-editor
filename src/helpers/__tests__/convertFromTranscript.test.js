bcc8eaebfa301b526e5b06227c59756a13b26434
Alex Norton <alexnorton@fastmail.com>
2017-07-11 15:47:42 +0100

    add convertFromTranscript and convertToTranscript tests

---
+++ src/helpers/__tests__/convertFromTranscript.test.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { Transcript } from 'transcript-model';
+import { convertToRaw } from 'draft-js';
+
+import convertFromTranscript from '../convertFromTranscript';
+
+describe('convertFromTranscript()', () => {
+  it('converts transcript to contentState and speakers', () => {
+    const transcriptJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'transcript.json')));
+    const transcript = Transcript.fromJSON(transcriptJson);
+
+    const { contentState, speakers } = convertFromTranscript(transcript);
+
+    const raw = convertToRaw(contentState);
+
+    Object.keys(raw.entityMap).forEach((key) => {
+      delete raw.entityMap[key].data.id;
+    });
+
+    expect(raw).toEqual(
+      JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'raw.json')))
+    );
+
+    expect(speakers.toJSON()).toEqual(
+      JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'speakers.json')))
+    );
+  });
+});
---
+++ src/helpers/__tests__/convertToTranscript.test.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { convertFromRaw } from 'draft-js';
+import { List } from 'immutable';
+
+import convertToTranscript from '../convertToTranscript';
+
+describe('convertToTranscript()', () => {
+  it('converts contentState and speakers to transcript', () => {
+    const raw = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'raw.json')));
+    const speakers = new List(JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'speakers.json'))));
+
+    const contentState = convertFromRaw(raw);
+
+    const transcript = convertToTranscript(contentState, speakers);
+
+    expect(transcript.toJSON()).toEqual(
+      JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'transcript.json')))
+    );
+  });
+});
